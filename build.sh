3b432f5bfe249f78e48c477a070c1576761e03d2
Philipp Heckel <pheckel@datto.com>
2021-08-19 17:43:01 +0200

    --no-cache, tmux+socat

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 echo "### Building $1 ..."
 cd $1
-docker build --tag binwiederhier/$1:latest .
+#docker build --tag binwiederhier/$1:latest .
+docker build --no-cache --tag binwiederhier/$1:latest .
 
